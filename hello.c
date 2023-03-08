8decd9e8e315a10359d0cf1352cd8fb0f475cc83
kush905 <32181432+kush905@users.noreply.github.com>
2023-03-08 14:23:15 +0600

    удалено лишнее

--- hello.c
+++ hello.c
@@ -19,48 +19,43 @@ int main() {
 
 
 int HoCo(const char * str) {
-    
     int counter = 0;
-    int varSym = 1; //set value for '&'
 
     for(int i = 0;str[i];i++) {
         switch (str[i])
         {
-        case '&':
-            counter = counter + varSym;//variable value of & (1 or 2)
-            break;
-
-        //1 hole
-        case '#':
-        case '0':
-        case '4':
-        case '6':
-        case '9':
-        case '@':
-        case 'A':
-        case 'a':
-        case 'b':
-        case 'D':
-        case 'd':
-        case 'e':
-        case 'g':
-        case 'o':
-        case 'O':
-        case 'p':
-        case 'P':
-        case 'q':
-        case 'Q':
-        case 'R': counter++;
-            break;
+            //1 hole
+            case '&':
+            case '#':
+            case '0':
+            case '4':
+            case '6':
+            case '9':
+            case '@':
+            case 'A':
+            case 'a':
+            case 'b':
+            case 'D':
+            case 'd':
+            case 'e':
+            case 'g':
+            case 'o':
+            case 'O':
+            case 'p':
+            case 'P':
+            case 'q':
+            case 'Q':
+            case 'R': counter++;
+                break;
         
-        //2 holes
-        case '$':
-        case '%':
-        case 'B':
-        case '8': counter=+2;
-            break;
-        default:
-            break;
+            //2 holes
+            case '$':
+            case '%':
+            case 'B':
+            case '8': counter=+2;
+                break;
+            default:
+                break;
         }
     }
     return counter;
